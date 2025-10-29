b81c1ff13509a31ffb5f5488278357c429370ceb
kamlesh <kamlesh81473@gmail.com>
2025-10-29 16:27:19 +0530

    add new data

---
+++ react-app/src/ProductTab.jsx
@@ -0,0 +1,11 @@
+import Product from "./Product.jsx";
+function ProductTab() {
+    return (
+        <>
+        <Product title="phone" price={30000} />
+        <Product title="laptop" price={50000}/>
+        <Product title="tablet" price={35000}/>
+        </>
+    );
+}
+export default ProductTab;
